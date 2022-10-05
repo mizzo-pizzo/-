40cc755a5053ae6738dbb61977c93ccf73f841a1
mizzo-pizzo <114093387+mizzo-pizzo@users.noreply.github.com>
2022-10-05 17:52:37 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+int main() {
+	/* // задача 1
+	   int hp;
+	   cin >> hp;
+	   if (hp > 0) {//true
+		   cout << "true";
+	   }
+	   else if (hp < 0) {//false
+		   cout << " false";
+	   }
+	   else if (hp == 0) {//false
+		   cout << "false";
+	   } */
+
+	/*   //задача 2
+	int temp;
+	cin >> temp;
+	if ( temp == 20) {//true
+		cout <<  "on";
+	}
+	else if (temp < 20) {//false
+		cout << "off";
+	} */
+
+
+
+
+	/* //задача 4
+	int y, x, f;
+	cin >> y, x, f;
+	if (x > 0) y = f = x - 2;
+	else if (x == 0) y = f = 0;
+	else if (x < 0) y = f = x;
+	cout << "f(x) =" << y << endl; */
+
+/*	//задача 5
+	int finger;
+	cin >> finger;
+	if (finger == 1) {//true
+		cout << "thumb";
+	}
+	if (finger == 2) {//true
+		cout << "index";
+	}
+	if (finger == 3) {//true
+				cout << "middle";
+			}
+	if (finger == 4) {//true
+				cout << "ring";
+			}
+	if (finger == 5) {//true
+		cout << "pinky";
+		if (finger < 10) {//false
+			cout << "error";
+		} */
+
+
+
+	}
